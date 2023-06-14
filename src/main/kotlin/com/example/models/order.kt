c0727bea58bdec7bdad889426d78e152c24c511a
Tema <92046258+VegaMethadone@users.noreply.github.com>
2023-06-14 23:26:27 +0300

    Add files via upload

---
+++ src/main/kotlin/com/example/models/order.kt
@@ -0,0 +1,16 @@
+package com.example.models
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Order(
+    val city: String,
+    val address: String,
+    val dateOfDelivery: String,
+    val timeOfDelivery: String,
+    val ratClass: String,
+    val ratColor: String,
+    val skills: Array<String>
+)
+
+fun createOrder(O)
