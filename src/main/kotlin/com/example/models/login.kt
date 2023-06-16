08605df23072afcb8646636ac59c87a3ee98a4e0
Tema <92046258+VegaMethadone@users.noreply.github.com>
2023-06-16 23:35:59 +0300

    Add files via upload

---
+++ src/main/kotlin/com/example/models/login.kt
@@ -0,0 +1,42 @@
+package com.example.models
+
+import com.mongodb.client.model.Filters.eq
+import kotlinx.serialization.Serializable
+import org.litote.kmongo.KMongo
+import org.litote.kmongo.findOne
+import org.litote.kmongo.getCollection
+
+@Serializable
+data class LoginRequest(
+    val mail: String,
+    val password: String
+)
+
+@Serializable
+data class LoginResponse(
+    val token: String
+)
+
+@Serializable
+data class User(
+    val userMail: String?,
+    val userId: Int?
+)
+
+fun autorisation(login: LoginRequest): Boolean  {
+    val client  = KMongo.createClient()
+    val dataBase = client.getDatabase("Ratatui")
+    val col  = dataBase.getCollection<Person>("Persons")
+
+    val filter = eq("email", login.mail)
+    val result = col.findOne(filter)
+
+    var flag = false
+    if (result != null) {
+        if(result.password == login.password)
+        {
+            flag = true
+        }
+    }
+    return flag
+}
---
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -0,0 +1,15 @@
+package com.example.plugins
+
+import com.example.routes.login
+import com.example.routes.ordering
+import com.example.routes.registrationPerson
+import io.ktor.server.application.*
+import io.ktor.server.routing.*
+
+fun Application.configureRouting() {
+    routing {
+        registrationPerson()
+        login()
+        ordering()
+    }
+}
